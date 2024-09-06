61c0382fa0ae27ac303d20c4aca4504a5e3ea8e4
Sinchana Ananth <sinchana.ananth@gmail.com>
2024-09-05 23:31:53 -0400

    pages

---
+++ frontend/whfrontend/src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Navbar from "../assets/Navbar";
+
+function SignUp(){
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default SignUp;
