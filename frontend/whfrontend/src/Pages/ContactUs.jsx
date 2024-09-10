bd2096bbecb8f3ace6517bf88fefed4dc7adc495
Sinchana Ananth <sinchana.ananth@gmail.com>
2024-09-10 16:27:03 -0400

    contact us page

--- frontend/whfrontend/src/Pages/ContactUs.jsx
+++ frontend/whfrontend/src/Pages/ContactUs.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import Navbar from "../assets/Navbar";
+import "./ContactUs.css";
 
 function ContactUs(){
     return (
         <>
             <Navbar />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className = "contact">
+                <br></br>
+                <h className="heading">ANY QUESTIONS OR CONCERNS?</h>
+                <br></br>
+                <p className="text">Email us: inquiries@waterlink.ca</p>
+            </div>
         </>
     )
 }
